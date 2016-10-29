13a566da359fc8cef73b8c52e8846989282f539b
Peter <mach1inferno@yahoo.com>
2016-10-28 22:27:21 -0400

    Updated stuff

---
+++ Assets/Scripts/GrinderLabel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrinderLabel : MonoBehaviour {
+
+	private SpriteRenderer spriteRenderer;
+
+	[SerializeField]
+	private Sprite[] sprites = null;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+		spriteRenderer.sprite = sprites[PlayerPrefsManager.GetTheme()];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
